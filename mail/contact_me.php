c9081404bd9b03b4175fb33516233d15a13dd42d
hjvshim <40854471+hjvshim@users.noreply.github.com>
2018-08-06 16:45:13 +0900

    All functions are working now
    - mail function
    - portfolio
    - around the web link
    - changed the hosting company (to cafe24)

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,11 +11,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "yourname@yourdomain.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";	
+$to = "hj_veronica@naver.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = "포트폴리오 사이트 메일:  $name";
+$body = "당신의 개인 홈페이지에서 메일이 도착했습니다.\n\n"."Name: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$header = "From: noreply@hjveronica.cafe24.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header .= "Reply-To: $email";
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
